8ec867f7e3d2bc423be7e18e8fbdf69e7941703e
PanArnaud <a.panapadeatchy@hotmail.fr>
2022-06-19 00:23:52 +0400

    Update header

--- src/navigators/NewsNavigator.tsx
+++ src/navigators/NewsNavigator.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Button, Icon } from "@rneui/themed";
 import LatestNews from "../screens/LatestNews";
@@ -5,17 +6,28 @@ import LatestNews from "../screens/LatestNews";
 const Stack = createNativeStackNavigator();
 
 const NewsNavigator = (): JSX.Element => {
+  const { colors } = useTheme();
+
   return (
     <Stack.Navigator>
       <Stack.Screen
         name="latest-news"
         component={LatestNews}
         options={{
-          headerLeft: () => <Button type="clear">Sections</Button>,
-          headerRight: ({ tintColor }) => (
-            <Button type="clear">
-              <Icon name="search" color={tintColor} type="feather" />
-            </Button>
+          headerLeft: () => (
+            <Button
+              type="clear"
+              titleStyle={{ color: colors.primary }}
+              title="Sections"
+            />
+          ),
+          headerRight: () => (
+            <Button
+              type="clear"
+              icon={
+                <Icon name="search" type="feather" color={colors.primary} />
+              }
+            />
           ),
         }}
       />
