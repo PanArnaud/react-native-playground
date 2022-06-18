27b7b8b1375ae747ba571d2c5579cbd9586b5313
PanArnaud <a.panapadeatchy@hotmail.fr>
2022-06-19 00:26:06 +0400

    Update theme

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -1,7 +1,8 @@
 import { createTheme, darkColors, lightColors } from "@rneui/themed";
-import { Platform } from "react-native";
+import { Appearance, Platform } from "react-native";
 
-export const defaultMode = "light";
+const colorScheme = Appearance.getColorScheme();
+export const defaultMode = colorScheme === "dark" ? "dark" : "light";
 
 export const theme = createTheme({
   lightColors: {
@@ -13,7 +14,9 @@ export const theme = createTheme({
   darkColors: {
     ...Platform.select({
       default: darkColors.platform.android,
-      ios: darkColors.platform.ios,
+      ios: {
+        ...darkColors.platform.ios,
+      },
     }),
   },
   mode: defaultMode,
